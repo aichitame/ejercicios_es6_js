7e7c73cacb5fabb58b32ebe7b64b216960b9c18b
aichitame <159391105+aichitame@users.noreply.github.com>
2025-09-25 18:18:05 +0200

    ejercicio 7 hecho y subido

---
+++ ejercicio7.js
@@ -0,0 +1,20 @@
+// 7.1 Dado el siguiente javascript filtra los videojuegos por gender = 'RPG' usando .filter() y
+//usa .reduce() para conseguir la media de sus .score. La función .find() también podría ayudarte para el contrar el genero 'RPG' en el array .gender.
+
+const videogames = [
+    {name: 'Final Fantasy VII', genders: ['RPG'], score: 9.5},
+    {name: 'Assasins Creed Valhalla', genders: ['Aventura', 'RPG'], score: 4.5},
+    {name: 'The Last of Us 2', genders: ['Acción', 'Aventura'], score: 9.8},
+    {name: 'Super Mario Bros', genders: ['Plataforma'], score: 8.5},
+    {name: 'Genshin Impact', genders: ['RPG', 'Aventura'], score: 7.5},
+    {name: 'Legend of Zelda: Breath of the wild', genders: ['RPG'], score: 10},
+];
+
+const genderVideojuegos = videogames.filter(game => game.genders.includes('RPG'));
+console.log(genderVideojuegos);
+
+const mediaScore = videogames.reduce((acc, videogame) => acc + videogame.score, 0) / videogames.length;
+console.log(mediaScore);
+
+
+
